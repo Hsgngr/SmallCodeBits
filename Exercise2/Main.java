1faaa7c7eaf470170bb0eae777073df27d562ed0
Ege <32769732+Hsgngr@users.noreply.github.com>
2019-10-02 14:39:48 +0100

    Add files via upload

---
+++ Exercise2/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        function someFunc = new function();
+        System.out.println(someFunc.functionString("selamunhello"));
+    }
+}
